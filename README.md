5dfaf3a5966f9d138fc5591fc6fcffe5fa334871
ipg74 <34241479+ipg74@users.noreply.github.com>
2023-02-20 21:29:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-expenses
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jfjpvf)
---
+++ src/app/category-list/category-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { categories } from '../categories';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css'],
+})
+export class CategoryListComponent {
+  categories = categories;
+
+  share() {
+    window.alert('The categories has been shared!');
+  }
+}
